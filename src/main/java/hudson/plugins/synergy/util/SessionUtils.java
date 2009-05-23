98ea127087e6c9747f75b28e6b5f51a462d84afa
jribette <jribette@dev.java.net>
2009-05-23 11:23:40 +0000

    Don't check for already opened session if not asked too. Don't fail with NPE in stop if failed to start

--- src/main/java/hudson/plugins/synergy/util/SessionUtils.java
+++ src/main/java/hudson/plugins/synergy/util/SessionUtils.java
@@ -49,30 +49,37 @@ public class SessionUtils {
 	 */
 	public static Commands openSession(FilePath path, SynergySCM synergySCM, TaskListener listener, Launcher launcher) throws IOException, InterruptedException, SynergyException {
 		Commands commands = configureCommands(synergySCM, listener, launcher);
-		
+		String ccmAddr = CheckSessionCommand.SESSION_NOT_FOUND;
 		FilePath ccmSessionMapFile = new FilePath(path, SynergySCM.CCM_SESSION_MAP_FILE_NAME);
-
-		CheckSessionCommand checkSessionCommand = new CheckSessionCommand();
-		commands.executeSynergyCommand(path, checkSessionCommand);
-		String ccmAddr = checkSessionCommand.getCcmAddr(ccmSessionMapFile);
-		if (CheckSessionCommand.SESSION_NOT_FOUND.equals(ccmAddr)) {			
-			// Get Synergy parameters.
-			String database = synergySCM.getDatabase();
-			String username = synergySCM.getUsername();
-			String password = synergySCM.getPassword();
-			boolean remoteClient = synergySCM.isRemoteClient();
-			String pathName = synergySCM.getDescriptor().getPathName();
-			String engine = synergySCM.getEngine();
-			
-			// Start Synergy.
-			StartCommand startCommand = new StartCommand(database, engine, username, password, remoteClient, pathName);
-			commands.executeSynergyCommand(path, startCommand);
-			ccmAddr = startCommand.getCcmAddr();
-			startCommand.addCcmAddrToSessionMapFile(ccmSessionMapFile);
+		if (synergySCM.isLeaveSessionOpen()) {
+			CheckSessionCommand checkSessionCommand = new CheckSessionCommand();
+			commands.executeSynergyCommand(path, checkSessionCommand);
+			ccmAddr = checkSessionCommand.getCcmAddr(ccmSessionMapFile);
 		}
+		if (CheckSessionCommand.SESSION_NOT_FOUND.equals(ccmAddr)) {			
+			ccmAddr = startSession(path, synergySCM, commands, ccmSessionMapFile);
+		}		
 		commands.setCcmAddr(ccmAddr);
 		
 		return commands;
+	}
+
+	private static String startSession(FilePath path, SynergySCM synergySCM, Commands commands, FilePath ccmSessionMapFile) throws IOException, InterruptedException, SynergyException {
+		String ccmAddr;
+		// Get Synergy parameters.
+		String database = synergySCM.getDatabase();
+		String username = synergySCM.getUsername();
+		String password = synergySCM.getPassword();
+		boolean remoteClient = synergySCM.isRemoteClient();
+		String pathName = synergySCM.getDescriptor().getPathName();
+		String engine = synergySCM.getEngine();
+		
+		// Start Synergy.
+		StartCommand startCommand = new StartCommand(database, engine, username, password, remoteClient, pathName);
+		commands.executeSynergyCommand(path, startCommand);
+		ccmAddr = startCommand.getCcmAddr();
+		startCommand.addCcmAddrToSessionMapFile(ccmSessionMapFile);
+		return ccmAddr;
 	}
 	
 	/**
@@ -84,10 +91,12 @@ public class SessionUtils {
 	 * @throws InterruptedException
 	 * @throws SynergyException
 	 */
-	public static void closeSession(FilePath path, SynergySCM synergySCM, Commands commands) throws IOException, InterruptedException, SynergyException {
+	public static void closeSession(FilePath path, SynergySCM synergySCM, Commands commands) throws IOException, InterruptedException, SynergyException {		
 		if (!synergySCM.isLeaveSessionOpen()) {
-			StopCommand stopCommand = new StopCommand();
-			commands.executeSynergyCommand(path, stopCommand);
+			if (commands!=null) {
+				StopCommand stopCommand = new StopCommand();
+				commands.executeSynergyCommand(path, stopCommand);
+			}
 		}
 	}
 }
