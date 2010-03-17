59ffdac9fc2c31509663204f3b28505c1b9e91ab
scoopex <scoopex@dev.java.net>
2010-03-17 15:44:44 +0000

    Repaired TaskInfoCommand and created testcases for 6.5 and 7.1

--- src/main/java/hudson/plugins/synergy/impl/TaskInfoCommand.java
+++ src/main/java/hudson/plugins/synergy/impl/TaskInfoCommand.java
@@ -22,7 +22,10 @@ import java.util.regex.Matcher;
  * TODO : work with several task at once.
  */
 public class TaskInfoCommand extends Command {
-	private DateFormat synergyFormat = new SimpleDateFormat("EE MMM dd hh:mm:ss yyyy", Locale.ENGLISH);
+
+	private DateFormat synergyDateFormat65 = new SimpleDateFormat("EE MMM dd hh:mm:ss yyyy", Locale.ENGLISH);
+	// 3/17/10 7:55 AM
+	private DateFormat synergyDateFormat71 = new SimpleDateFormat("MM/dd/yy hh:mm a", Locale.ENGLISH);
 	
 	private List<String> tasks;
 	private List<TaskCompleted> informations;
@@ -41,17 +44,22 @@ public class TaskInfoCommand extends Command {
 	public void parseResult(String result) {
 		informations = new ArrayList<TaskCompleted>();
 		TaskCompleted task = null;
-		Pattern re_taskid = Pattern.compile("^.+?([0-9]+).+$");
+		Pattern re_taskid = Pattern.compile("^\\s*Task.+?([0-9]+)[^\\d]*$");
+		Pattern re_resolver = Pattern.compile("^\\s*Resolver:\\s*(.*)\\s*$");
+		Pattern re_completiondate = Pattern.compile("^\\s*Actual Completion Date:\\s*(.*)\\s*$");
 		try {
 			BufferedReader reader = new BufferedReader(new StringReader(result));
 			String line = reader.readLine();
 			while (line!=null) {
 			   // TODO: check the format of the taskid-line in releases < 7.1
-			   Matcher m = re_taskid.matcher(line);
-				if (m.matches()){
+			   Matcher m_taskid = re_taskid.matcher(line);
+			   Matcher m_resolver = re_resolver.matcher(line);
+			   Matcher m_completiondate = re_completiondate.matcher(line);
+
+				if (m_taskid.matches()){
 					task = new TaskCompleted();
 					informations.add(task);
-					task.setId(m.group(1));
+					task.setId(m_taskid.group(1));
 				}else if (line.indexOf("Synopsis:")!=-1) {
 					// Check whether we have already stored task synopsis
 					// (in case task description also contains 'Synopsis:')
@@ -59,20 +67,30 @@ public class TaskInfoCommand extends Command {
 						// TODO multiline synopsis
 						task.setSynopsis(line.substring(line.indexOf(':')+1).trim());
 					}
-				}else if (line.indexOf("Resolver:")!=-1) {
-					task.setResolver(line.substring(line.indexOf(':')+1).trim());
-				} else if ((line.indexOf("Status set to 'completed'")!=-1) ||
-						     (line.indexOf("Actual Completion Date:")!=-1)){ 
+				}else if (m_resolver.matches()){
+					task.setResolver(m_resolver.group(1));
+				}else if (line.indexOf("Status set to 'completed'")!=-1){
 					try {
-						String dateAsString = line.substring(0, line.lastIndexOf(':'));
-						Date date = synergyFormat.parse(dateAsString);
+						String dateAsString = line.substring(0, line.lastIndexOf(':')); 
+						Date date = synergyDateFormat65.parse(dateAsString);
 						task.setDateCompleted(date);
 					} catch (ParseException e) {
 						// ignore.
 						// TODO: log parsing problems to hudson logfile
 					  System.out.println("ParseException: '"+line+"'");
 					}
+				}else if (m_completiondate.matches()){
+					String dateAsString = m_completiondate.group(1);
+					try {
+						Date date = synergyDateFormat71.parse(dateAsString);
+						task.setDateCompleted(date);
+					} catch (ParseException e) {
+						// ignore.
+						// TODO: log parsing problems to hudson logfile
+					  System.out.println("ParseException: '"+dateAsString+"'");
+					}
 			   }
+
 				line = reader.readLine();				
 			}
 		} catch (IOException e) {
--- src/test/java/hudon/plugins/synergy/test/log/ReadTaskInfoLogTest.java
+++ src/test/java/hudon/plugins/synergy/test/log/ReadTaskInfoLogTest.java
@@ -12,7 +12,7 @@ public class ReadTaskInfoLogTest extends AbstractLogTest {
 	/**
 	 * Tests the task info log 1.
 	 */
-	public void testLog1() throws IOException {
+	public void testTaskInfoCommandSyngery65() throws IOException {
 		// Test a Synergy 6.5 task info log.
 		TaskInfoCommand taskCommand = new TaskInfoCommand(new ArrayList<String>());
 		String log = readLog("logs/tasks/task1.log.txt");
@@ -39,4 +39,36 @@ public class ReadTaskInfoLogTest extends AbstractLogTest {
 		assertEquals("Ajout LNG_CODE vue interface uti", tasks.get(0).getSynopsis());
 		assertEquals(cal.getTime(), tasks.get(0).getDateCompleted());
 	}
+	/**
+	 * Tests the task info log 2.
+	 */
+	public void testTaskInfoCommandSynergy71() throws IOException {
+		// Test a Synergy 7.1 task info log.
+		TaskInfoCommand taskCommand = new TaskInfoCommand(new ArrayList<String>());
+		String log = readLog("logs/tasks/task2.log.txt");
+		taskCommand.parseResult(log);
+		List<TaskCompleted> tasks = taskCommand.getInformations();
+		
+		// Parsing of the log should not be null.
+		assertNotNull(tasks);
+		
+		// There are 1 task in this log.
+		assertEquals(1,tasks.size());
+	
+		// Sat Apr 17 07:55:32 CEST 2010
+		// Check the task elements.
+		Calendar cal = Calendar.getInstance();
+		cal.clear();
+		cal.set(Calendar.YEAR, 2010);
+		cal.set(Calendar.MONTH, 2);
+		cal.set(Calendar.DAY_OF_MONTH, 17);
+		cal.set(Calendar.HOUR_OF_DAY, 7);
+		cal.set(Calendar.MINUTE, 55);
+
+		assertEquals("462", tasks.get(0).getId());
+		assertEquals("BacAn2d", tasks.get(0).getResolver());
+		assertEquals("JavaDoc Foobar (hehehe) gnarf?", tasks.get(0).getSynopsis());
+		assertEquals(cal.getTime(), tasks.get(0).getDateCompleted());
+	}
+
 }
