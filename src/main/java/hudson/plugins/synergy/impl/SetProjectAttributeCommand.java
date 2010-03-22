48a4fcc842eb48d5b468e75d65cddd00a8e48063
scoopex <scoopex@dev.java.net>
2010-03-22 15:32:14 +0000

    Added a todo/comment which explains problems with setting the maintain_wa attribute

--- src/main/java/hudson/plugins/synergy/impl/SetProjectAttributeCommand.java
+++ src/main/java/hudson/plugins/synergy/impl/SetProjectAttributeCommand.java
@@ -15,6 +15,18 @@ public class SetProjectAttributeCommand extends Command {
 	}
 	@Override
 	public String[] buildCommand(String ccmExe) {	
+		/* 
+		 * TODO: 
+		 * Due to a bug in Synergy 7, setting attribute maintain_wa by "ccm attr" does not work 
+		 * ("cmm attr -m maintain_wa -v TRUE/FALSE -project foobar_int_1.1.1")
+		 * 
+		 *
+		 * This might be fixed in the next weeks, this command might by splitted in two commands:
+		 * "ccm wa -set $HOME/ccm_wa @20"
+		 * "ccm wa -nwa/-wa -project <projectname>" 
+		 *
+		 * Executing the last command manually can be used as a workaround.
+		 */
 		String[] commands = new String[]{ccmExe, "attr", "-m", attribute, "-v", value, "-project", project};
 		return commands;		
 	}
