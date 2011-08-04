1b48967e7c04e190f571cd4ea87bfe71d307ad88
Keith Mendoza <pantherse@gmail.com>
2011-08-04 08:35:52 -0700

    Needed to trim the release and purpose string values (fixes JENKINS-8613)
    Added a test case while I'm at it
    
    Closes #6

--- src/main/java/hudson/plugins/synergy/impl/CreateProjectBaselineCommand.java
+++ src/main/java/hudson/plugins/synergy/impl/CreateProjectBaselineCommand.java
@@ -1,13 +1,17 @@
-package hudson.plugins.synergy.impl;
+ package hudson.plugins.synergy.impl;
 
 import java.util.Arrays;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreateProjectBaselineCommand extends Command {
 	private String name;
 	private String project;
 	private String release;
 	private String purpose;
+	
+	private static final Logger LOGGER = Logger.getLogger(CreateProjectBaselineCommand.class.getName());
 		
 	public CreateProjectBaselineCommand(String name, String project, String release, String purpose) {
 		this.name = name;
@@ -18,12 +22,12 @@ public class CreateProjectBaselineCommand extends Command {
 	@Override
 	public String[] buildCommand(String ccmExe) {
 		Vector<String> commands = new Vector<String>(Arrays.asList(new String[]{ccmExe, "baseline", "-create", name, "-p", project }));
-		if(this.release != "") {
+		if(this.release.trim() != "") {
 			commands.add("-r");
 			commands.add(release);
 		}
 		
-		if(this.purpose != "") {
+		if(this.purpose.trim() != "") {
 			commands.add("-purpose");
 			commands.add(purpose);	
 		}
